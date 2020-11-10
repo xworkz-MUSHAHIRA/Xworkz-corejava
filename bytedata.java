821850ff8c9e2edb2481944028693e9f421ea990
xworkz-MUSHAHIRA <mushahiraece.xworkz@gmail.com>
2020-11-10 19:09:41 +0530

    Adding : bytedata file

---
+++ bytedata.java
@@ -0,0 +1,14 @@
+class ByteDataType
+{
+	public static void main (String a[])
+	{
+	byte noofchairs = 50;
+	byte age = -128;
+	byte glass = 127 ;
+	
+	System.out.println(noofchairs +"\n");
+	System.out.println(age +"\n");
+	System.out.println(glass);
+	}
+}
+ // editing the file to  modify in repository 
